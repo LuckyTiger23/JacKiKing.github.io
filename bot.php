e0ea98bf0efe488bb48282f274e6a465b9fdf83a
JacKiKing <32509854+JacKiKing@users.noreply.github.com>
2022-07-18 22:36:28 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,25 +1,28 @@
 <?php
     const TOKEN = '5462005495:AAHMvWfZrFYd4dR6mrSYg_A7LRJr4lxQB2M';
-//    const BASE_URL
-    echo $url = 'https://api.telegram.org/bot'.TOKEN.'/getUpdates';
+    const BASE_URL = 'https://api.telegram.org/bot'.TOKEN.'/';
+    
 
-//    function sendRequest($method, $params = [])
-//    {
-//        if (!empty($params)){
-//            $url = BASE_URL . $method . '?' . http_build_query($params)
-//        } else {
-//            $url = BASE_URL . $method;
-//        }
-//        return json_decode(
-//            file_get_contents($url),
-//            JSON_OBJECT_AS_ARRAY
-//        );
-//    }
-//
-//    $updates = sendRequest('getUpdates');
-//
-//    foreach ($updates['result'] as $update) {
-//        $chat_id = $update['message']['chat']['id'];
-//        sendReuest('sendMessage', ['chat_id' => $chat_id, 'text' => 'answer']);
-//    }
+    function sendRequest($method, $params = [])
+    {
+        if (!empty($params)){
+            $url = BASE_URL . $method . '?' . http_build_query($params);
+        } else {
+            $url = BASE_URL . $method;
+        }
+        return json_decode(
+            file_get_contents($url),
+            JSON_OBJECT_AS_ARRAY
+        );
+    }
 
+    $updates = sendRequest('getUpdates');
+
+    
+    foreach ($updates['result'] as $update) {
+        $chat_id = $update['message']['chat']['id'];
+        sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Привет Ирина!']);
+    }
+
+
+?>
